d26b85f845286a45d397ce509edf2f9b290f323d
Bill Cui <billcui2001@hotmail.com>
2020-01-25 15:31:18 -0500

    Initial commit

---
+++ matching.py
@@ -0,0 +1,154 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name(
+    "MathSoc Job Matching-0e05c076907d.json", scope)
+
+client = gspread.authorize(creds)
+
+spreadSheet = client.open("Matching Data")
+
+worksheets = spreadSheet.worksheets()
+
+interviewers = []
+
+
+#Reads in all the interviewers
+
+for x in worksheets[:-1]:
+    interviewers.append(
+        {
+            "name": x.cell(2, 1).value,
+            "program": x.cell(2, 2).value,
+            "pastjobs": x.col_values(3)[1:],
+            "interviewees": [],
+            "intervieweesStudNumber": [],
+            "intervieweesEmail": [],
+            "researchAssistantRate": 0,
+            "teacherRate": 0,
+            "financialAnalystRate": 0,
+            "statisticianRate": 0,
+            "softDeveloperRate": 0,
+            "machineLearningInternRate": 0,
+            "dataScientistRate": 0,
+            "accountantRate": 0,
+            "businessRepRate": 0,
+            "mathModelerRate": 0,
+            "marketAssistantRate": 0,
+            "managerRate": 0
+        }
+    )
+
+
+# Gives rating to the level of expertise for each interviewer in each skill
+# TODO: Let major affect level too
+
+for interviewer in interviewers:
+    for job in interviewer["pastjobs"]:
+        if job.lower() == "Research Assistant".lower():
+            interviewer["researchAssistantRate"] += 1
+        elif job.lower() == "Teacher".lower():
+            interviewer["teacherRate"] += 1
+        elif job.lower() == "Financial Analyst".lower():
+            interviewer["financialAnalystRate"] += 1
+        elif job.lower() == "Statistician".lower():
+            interviewer["statisticianRate"] += 1
+        elif job.lower() == "Software Developer".lower():
+            interviewer["softDeveloperRate"] += 1
+        elif job.lower() == "Machine Learning Intern".lower():
+            interviewer["machineLearningInternRate"] += 1
+        elif job.lower() == "Data Scientist".lower():
+            interviewer["dataScientistRate"] += 1
+        elif job.lower() == "Accountant".lower():
+            interviewer["accountantRate"] += 1
+        elif job.lower() == "Business Representative".lower():
+            interviewer["businessRepRate"] += 1
+        elif job.lower() == "Mathematical Modeler".lower():
+            interviewer["mathModelerRate"] += 1
+        elif job.lower() == "Marketing Assistant".lower():
+            interviewer["marketAssistantRate"] += 1
+        elif job.lower() == "Manager".lower():
+            interviewer["managerRate"] += 1
+        else:
+            print("Error parsing", job)
+     
+
+
+
+#Reads in interviewees
+
+interviewees = []
+
+formSheet = worksheets[len(worksheets)-1]
+
+for row in range(1,formSheet.row_count):
+
+    if not formSheet.cell(row+1, 3).value:
+        break
+    
+    interviewees.append(
+        {
+            "name": formSheet.cell(row + 1, 3).value + " " + formSheet.cell(row + 1, 4).value,
+            "email": formSheet.cell(row + 1, 2).value,
+            "studentNum": int(formSheet.cell(row + 1, 5).value),
+            "program ": formSheet.cell(row + 1, 6).value,
+            int (formSheet.cell(row + 1, 7).value) : "researchAssistantRate",
+            int (formSheet.cell(row + 1, 8).value) : "teacherRate",
+            int (formSheet.cell(row + 1, 9).value) : "financialAnalystRate",
+            int (formSheet.cell(row + 1, 10).value): "statisticianRate",
+            int (formSheet.cell(row + 1, 11).value): "softDeveloperRate",
+            int (formSheet.cell(row + 1, 12).value): "machineLearningInternRate",
+            int (formSheet.cell(row + 1, 13).value): "dataScientistRate",
+            int (formSheet.cell(row + 1, 14).value): "accountantRate",
+            int (formSheet.cell(row + 1, 15).value): "businessRepRate",
+            int (formSheet.cell(row + 1, 16).value): "mathModelerRate",
+            int (formSheet.cell(row + 1, 17).value): "marketAssistantRate",
+            int (formSheet.cell(row + 1, 18).value): "managerRate",
+        }
+    )
+    
+
+#Max capacity of each interviewer
+#TODO make it more robust
+maxCapacity = len(interviewees) / len(interviewers) + 5
+
+#Algorithm for matching
+for interviewee in interviewees:
+    desiredJobRank = 1
+    matchedInterviewer = False
+    while True:
+        for interviewer in interviewers:
+            if (interviewer[interviewee[desiredJobRank]] > 2) and (len(interviewer["interviewees"])) < maxCapacity:
+                interviewer["interviewees"].append(interviewee["name"])
+                interviewer["intervieweesStudNumber"].append(interviewee["studentNum"])
+                interviewer["intervieweesEmail"].append(interviewee["email"])
+                matchedInterviewer = True
+                break
+            else:
+                continue
+               
+        
+        if not matchedInterviewer:
+            desiredJobRank += 1
+        else:   
+            break
+             
+             
+#Pushes changes back
+
+for worksheet in worksheets[:-1]:
+    interviewerIndex = worksheets.index(worksheet)
+    for row in range(1,len(interviewers[interviewerIndex]["interviewees"])+1):
+        worksheet.update_cell(row+1,4,interviewers[interviewerIndex]["interviewees"][row-1])
+        worksheet.update_cell(row+1,5,interviewers[interviewerIndex]["intervieweesStudNumber"][row-1])
+        worksheet.update_cell(row+1,6,interviewers[interviewerIndex]["intervieweesEmail"][row-1])
+    
+
+
+
+
+
